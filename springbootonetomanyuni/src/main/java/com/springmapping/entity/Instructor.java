9b8a5efc2c32a33a64659503a5f73a5ffc697b92
logeshhub07 <logesh.7871ll@gmail.com>
2023-09-23 11:43:22 +0530

    one to many unidirectional mapping

---
+++ springbootonetomanyuni/src/main/java/com/springmapping/entity/Instructor.java
@@ -0,0 +1,130 @@
+package com.springmapping.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetails instructorDetails;
+
+	@OneToMany(mappedBy = "instructor", 
+			fetch = FetchType.LAZY,
+			cascade = { CascadeType.DETACH, CascadeType.REFRESH, CascadeType.MERGE,
+			CascadeType.PERSIST })
+	private List<Course> courses;
+
+	public Instructor() {
+		super();
+	}
+
+	public Instructor(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGmail() {
+		return email;
+	}
+
+	public void setGmail(String gmail) {
+		this.email = gmail;
+	}
+
+	public InstructorDetails getInstructorDetails() {
+		return instructorDetails;
+	}
+
+	public void setInstructorDetails(InstructorDetails instructorDetails) {
+		this.instructorDetails = instructorDetails;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", gmail=" + email
+				+ ", instructorDetails=" + instructorDetails + "]";
+	}
+	
+	
+	
+	public void add(Course tempCourse) {
+		if(courses==null) {
+			courses=new ArrayList<>();
+		}
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
+	}
+
+}
